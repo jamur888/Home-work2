dc0ef0bb1bacbc893607dfced19f759328c0d790
Джамшит Мурадов <Jamur888@github.com>
2026-06-20 15:26:53 +0300

    2

--- src/Main.java
+++ src/Main.java
@@ -10,30 +10,16 @@ public class Main {
         try {
 
             ObjectMapper mapper = new ObjectMapper();
-            List<Student> students = mapper.readValue(
-                    new File("students.json"),
-                    new TypeReference<List<Student>>() {
-                    }
-            );
+            List<Student> students = mapper.readValue(new File("students.json"), new TypeReference<List<Student>>() {
+            });
 
             System.out.println("--- Выполнение стрима ---");
 
-            Optional<Integer> resultYear = students.stream()
-                    .peek(System.out::println)
-                    .map(Student::getBooks)
-                    .flatMap(List::stream)
-                    .sorted()
-                    .distinct()
-                    .filter(book -> book.getYear() > 2000)
-                    .limit(3)
-                    .map(Book::getYear)
-                    .findFirst();
+            Optional<Integer> resultYear = students.stream().peek(System.out::println).map(Student::getBooks).flatMap(List::stream).sorted().distinct().filter(book -> book.getYear() > 2000).limit(3).map(Book::getYear).findFirst();
 
             System.out.println("\n--- Результат Optional ---");
 
-            System.out.println(resultYear
-                    .map(year -> "Найден год выпуска книги: " + year)
-                    .orElse("Такая книга отсутствует"));
+            System.out.println(resultYear.map(year -> "Найден год выпуска книги: " + year).orElse("Такая книга отсутствует"));
 
         } catch (IOException e) {
             System.err.println("Ошибка чтения файла: " + e.getMessage());
